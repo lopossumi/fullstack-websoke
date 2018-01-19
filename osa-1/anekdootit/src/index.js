460738464478006ef8db68c0540c524f42e799fd
Mikko Loponen <mikko.loponen@iki.fi>
2018-01-19 19:33:50 +0200

    Tehtävä 13: äänestys, ja vähän CSS:ää

--- osa-1/anekdootit/src/index.js
+++ osa-1/anekdootit/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             selected: 0,
+            votes: new Array(anecdotes.length).fill(0)
         }
     }
 
@@ -15,19 +17,35 @@ class App extends React.Component {
         })
     }
 
+    vote = () => {
+        const newVotes = this.state.votes
+        newVotes[this.state.selected]++
+        this.setState({
+            votes: newVotes
+        })
+    }
+
     render() {
         return (
             <div>
-                <h1>Programming Anecdotes</h1>
-                <blockquote>{this.props.anecdotes[this.state.selected]}</blockquote>
                 <button onClick={this.randomQuote}>Next anecdote</button>
+                <table>
+                    <tbody>
+                        <tr><td><div className="votes">{this.state.votes[this.state.selected]}<br />
+                            <button onClick={this.vote}>Vote</button></div></td>
+                            <td>
+                                <blockquote>{this.props.anecdotes[this.state.selected]}</blockquote>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
 }
 
 const anecdotes = [
-    'If it hurts, do it more often',
+    'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
