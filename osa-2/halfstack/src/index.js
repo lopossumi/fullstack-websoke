2041e6d0c5199c4f0f42d48bb4f6a2ecfe22857b
lopossumi <mikko.loponen@iki.fi>
2018-01-22 10:51:44 +0200

    Tehtävä 2.4: monta kurssia

--- osa-2/halfstack/src/index.js
+++ osa-2/halfstack/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
-const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
+const Otsikko = (props) => <h2>{props.kurssi.nimi}</h2>
 
 const Sisalto = (props) => {
     const osat = () => props.kurssi.osat.map(
@@ -20,7 +20,7 @@ const Yhteensa = (props) => {
         .reduce((a, b) => a + b)
 
     return (
-        <p>yhteensä {summa} tehtävää</p>
+        <p>Yhteensä {summa} tehtävää</p>
     )
 }
 
@@ -35,30 +35,54 @@ const Kurssi = (props) => {
 }
 
 const App = () => {
-    const kurssi = {
-        nimi: 'Half Stack -sovelluskehitys',
-        osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10,
-                id: 1
-            },
-            {
-                nimi: 'Tiedonvälitys propseilla',
-                tehtavia: 7,
-                id: 2
-            },
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14,
-                id: 3
-            }
-        ]
-    }
-
+    const kurssit = [
+        {
+            nimi: 'Half Stack -sovelluskehitys',
+            id: 1,
+            osat: [
+                {
+                    nimi: 'Reactin perusteet',
+                    tehtavia: 10,
+                    id: 1
+                },
+                {
+                    nimi: 'Tiedonvälitys propseilla',
+                    tehtavia: 7,
+                    id: 2
+                },
+                {
+                    nimi: 'Komponenttien tila',
+                    tehtavia: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            nimi: 'Node.js',
+            id: 2,
+            osat: [
+                {
+                    nimi: 'Routing',
+                    tehtavia: 3,
+                    id: 1
+                },
+                {
+                    nimi: 'Middlewaret',
+                    tehtavia: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+    
+    
+    const rivit = () => kurssit.map(
+        kurssi => <Kurssi key={kurssi.id} kurssi = {kurssi} />)
+    
     return (
         <div>
-            <Kurssi kurssi={kurssi} />
+        <h1>Opetusohjelma</h1>
+            {rivit()}
         </div>
     )
 }
