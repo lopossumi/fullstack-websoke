5f6baffbd2f525ac6af682eef3d414512424616c
lopossumi <mikko.loponen@iki.fi>
2018-01-22 12:29:06 +0200

    Tehtävä 2.6

--- osa-2/puhelinluettelo/src/App.js
+++ osa-2/puhelinluettelo/src/App.js
@@ -13,7 +13,20 @@ class App extends React.Component {
 
     addName = (event) => {
         event.preventDefault()
-        console.log('clickclack')
+        const personObject = {
+            name: this.state.newName
+        }
+        const persons = this.state.persons.concat(personObject)
+
+        this.setState({
+            persons: persons,
+            newName: ''
+        })
+    }
+
+    handleNameChange = (event) => {
+        console.log(event.target.value)
+        this.setState({ newName:event.target.value})
     }
 
     nimilista = () => this.state.persons.map(
@@ -26,7 +39,10 @@ class App extends React.Component {
                 <h2>Puhelinluettelo</h2>
                 <form onSubmit={this.addName}>
                     <div>
-                        nimi: <input />
+                        nimi: 
+                        <input 
+                            value={this.state.newName}
+                            onChange={this.handleNameChange}/>
                     </div>
                     <div>
                         <button type="submit">lisää</button>
